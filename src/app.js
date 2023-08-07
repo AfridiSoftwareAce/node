1f5968c2babf8c011a2db7ca39c9b7b1060f48e1
AfridiSoftwareAce <afridiwara@ymail.com>
2023-08-07 20:46:38 +0530

    Assignment

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
+require('dotenv').config()
 const express = require('express');
 const { getUsersWithPostCount } = require('./controllers/user.controller');
-
+const { seed } = require('./helpers/seeder.helper'); // import the seed function
+const port=process.env.PORT || 3000
 const app = express();
+const { connect } = require('./helpers/db.helper'); // Import the connect function
+
+connect().then(async () => {
+    console.log('Connected to MongoDB')
+    await seed(); // call the seed function after connecting to MongoDB
+    console.log('Data seeding completed');
+}) 
+.catch(err => console.error('Failed to connect to MongoDB:', err));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/users', getUsersWithPostCount);
+
+app.listen(port, (err) => {
+    if (err) {
+        console.error('Error occurred while starting the server:', err);
+        return;
+    }
+    console.log(`Server successfully started and listening at http://localhost:${port}`);
+});
 
 module.exports = app;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,54 @@
+
 const User = require("../schema/user.schema");
+const Post = require("../schema/post.schema");
 
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
-    //TODO: Implement this API
+    const limit = Number(req.query.limit) || 10; // number of records per page
+    const page = Number(req.query.page) || 1; // page number
+    const skip = (page - 1) * limit; // skip 
 
-    res.status(200).json({});
+    const usersWithPostCount = await User.aggregate([
+      {
+        $lookup: {
+          from: Post.collection.name,
+          localField: '_id',
+          foreignField: 'userId',
+          as: 'userPosts'
+        }
+      },
+      {
+        $project: {
+          name: 1,
+          posts: { $size: "$userPosts" }
+        }
+      },
+      {
+        $skip: skip,
+      },
+      {
+        $limit: limit,
+      }
+    ]);
+
+    const totalDocs = await User.countDocuments();
+
+    const pagination = {
+      totalDocs: totalDocs,
+      limit: limit,
+      page: page,
+      totalPages: Math.ceil(totalDocs / limit),
+      pagingCounter: skip + 1,
+      hasPrevPage: page > 1,
+      hasNextPage: totalDocs - skip - limit > 0,
+      prevPage: page > 1 ? page - 1 : null,
+      nextPage: totalDocs - skip - limit > 0 ? page + 1 : null
+    }
+
+    res.status(200).json({ data: { users: usersWithPostCount, pagination: pagination } });
+
   } catch (error) {
-    res.send({ error: error.message });
+    res.status(500).send({ error: error.message });
   }
 };
+
